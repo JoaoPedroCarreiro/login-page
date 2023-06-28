721249ddcb7c0daf3ba5a85459d1637912e62aad
JoaoPedroCarreiro <jpcarreirof@gmail.com>
2023-06-28 17:02:52 -0300

    commit28-06-17

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,28 @@
-import StyledApp from "../styles/App.styled"
+import { lazy, Suspense } from "react"
+
 import StyledWelcome from "../styles/Welcome.styled"
+import Background from "./Background"
 
 import Enter from "./Enter"
 
+const StyledApp = lazy(() => import("../styles/App.styled"))
+
 function App() {
     return (
-        <StyledApp data-testid="app">
-            <StyledWelcome>
-                <div className="welcome-content">
-                    <h1>Welcome to Cherry</h1>
-                    <p>Here is where your dreams become true</p>
-                </div>
-            </StyledWelcome>
-            <Enter />
-        </StyledApp>
+        <>
+            <Background/>
+            <Suspense fallback={<></>}>
+                <StyledApp data-testid="app">
+                    <StyledWelcome>
+                        <div className="welcome-content">
+                            <h1>Welcome to Cherry</h1>
+                            <p>Here is where your dreams become true</p>
+                        </div>
+                    </StyledWelcome>
+                    <Enter />
+                </StyledApp>
+            </Suspense>
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,13 +5,5 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.min.css"
 
 import App from "./components/App"
-import Background from "./components/Background"
 
-const roots = {
-    "background": <Background />,
-    "app": <App />
-}
-
-for(const root in roots) {
-    ReactDOM.createRoot(document.getElementById(root)).render(roots[root])
-}
+ReactDOM.createRoot(document.getElementById("app")).render(<App />)
